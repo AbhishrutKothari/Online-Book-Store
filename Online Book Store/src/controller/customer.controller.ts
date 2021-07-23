54c24dd76634d96044549270dd74136e32667ce9
Dharshak Ch <dharshak.ch97@gmail.com>
2021-07-23 16:23:00 +0530

    user login done

--- Online Book Store/src/controller/customer.controller.ts
+++ Online Book Store/src/controller/customer.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import CustomerModel from "../model/customer.model";
 import BookModel from "../model/book.model";
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const getAllUsers = (req: Request, res: Response) => {
     CustomerModel.find({}, (err: any, doc: any) => {
         if (!err) {
@@ -24,16 +27,67 @@ const getUserById = (req: Request, res: Response) => {
 
 const createCustomer = (req: Request, res: Response) => {
     const customer = req.body;
+    customer.password = hashPassword(customer.password)
+    console.log(customer)
     const newCustomer = new CustomerModel(customer);
 
     newCustomer.save((err: any) => {
         if (!err) {
+            console.log('check')
             res.json({ message: "Inserted successfully" });
         } else {
             res.json(err);
         }
     });
 };
+
+const logUser = async (req: Request, res: Response) => {
+    const login = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    try {
+        let user = await CustomerModel.findOne({
+            email: login.email
+        })
+        console.log(user)
+        if (!user) {
+            res.status(400).json({
+                type: "Not found",
+                msg: "Wrong credentials"
+            })
+        }
+        console.log(user)
+        let match = compareUserPassword(login.password, user?.password)
+        console.log(user?.password)
+        if (match) {
+            let token = generateJwtToken({
+                user
+            }, "secret" , {
+                expiresIn: 10000
+            })
+            if (token) {
+                res.status(200).json({
+                    success: true,
+                    token: token,
+                    userCredentials: user
+                })
+            }
+        }
+        else {
+            res.status(400).json({
+                type: "Not found",
+                msg: "Incorrect credentials"
+            })
+        }
+    }
+    catch(err) {
+        res.status(500).json({
+            msg: err,
+            type: "Something went wrong"
+        })
+    }
+}
 
 const updateCustomerById = (req: Request, res: Response) => {
     let cid = req.params.id
@@ -61,6 +115,16 @@ const deleteCustomerById = (req: Request, res: Response) => {
         }
     });
 };
+
+let hashPassword =  (password:any) => {             // this function is use to convert our password in encript format. 
+    return bcrypt.hashSync(password, 10);          
+}
+let compareUserPassword = (inputtedPassword:any, hashedPassword:any) => {
+   return bcrypt.compare(inputtedPassword, hashedPassword)
+}
+let generateJwtToken = (payload:any, secret:any, expires:any) => {
+   return jwt.sign(payload, secret, expires)
+}
 
 const getAllBooks = (req: Request, res: Response) => {
     BookModel.find({}, (err: any, doc: any) => {
@@ -92,6 +156,6 @@ const getBookByAuthor = (req: Request, res: Response) => {
     });
 };
 
-export { getAllUsers, getUserById, createCustomer, updateCustomerById, deleteCustomerById, getAllBooks, getBookById, getBookByAuthor };
+export { getAllUsers, getUserById, createCustomer, logUser, updateCustomerById, deleteCustomerById, getAllBooks, getBookById, getBookByAuthor };
 
 
--- Online Book Store/src/router/auth.router.ts
+++ Online Book Store/src/router/auth.router.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import * as adminController from "../controller/admin.controller";
+import * as createCustomer from "../controller/customer.controller";
 
 export let router: Router = express(); 
 
 router.post("/adminreg", adminController.createUser)
 router.post("/adminlogin", adminController.logUser)
 
-router
+router.post("/userreg", createCustomer.createCustomer)
+router.post("/userlog", createCustomer.logUser)
